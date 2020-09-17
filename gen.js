0aca17e767b379c4ea16607ceb33542f5c43251e
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2020-09-17 12:39:36 +0200

    fix workflow config

--- gen.js
+++ gen.js
@@ -1,7 +1,15 @@
+const { log } = require("console");
+
+const { stringify } = require("querystring");
+
 console.log(
-    Array.from(
-        process.binding('uv')
-            .getErrorMap()
-            .entries()
-    )
+    "exports = module.exports = JSON.parse('",
+    JSON.stringify(
+        Array.from(
+            process.binding('uv')
+                .getErrorMap()
+                .entries()
+        )
+    ) + "')\n",
+    'exports.os = process.platform;console.log(exports)'
 )
