87fc6dba9b05c1173d5b4abec7fcc5c92223c428
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2020-09-17 12:57:26 +0200

    fix gen output

--- gen.js
+++ gen.js
@@ -3,13 +3,13 @@ const { log } = require("console");
 const { stringify } = require("querystring");
 
 console.log(
-    "exports = module.exports = JSON.parse('",
+    "exports = module.exports = JSON.parse('" +
     JSON.stringify(
         Array.from(
             process.binding('uv')
                 .getErrorMap()
                 .entries()
         )
-    ) + "')\n",
-    'exports.os = process.platform;console.log(exports)'
+    ) +
+    `')\nexports.os = "${process.platform}"`
 )
