f01a4abdaf198897d73f6f75fbd8c078a1b6e62d
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2020-09-17 12:15:25 +0200

    further automate comparisons

--- test/index.js
+++ test/index.js
@@ -1,22 +1,67 @@
-const { equal } = require('assert')
-const gha = require('./errnomaps_gha')
-const loc = require('./errnomaps_loc')
+const { equal, notEqual } = require('assert')
+const { readdirSync } = require('fs')
+const { resolve } = require('path')
+const dirsArg = process.argv[2] || ""
 
-for (const osName in gha) {
-    const a = gha[ osName ]
-    const b = loc[ osName ]
+// set default
+let dirs = [
+    resolve(__dirname, './darwin'),
+    resolve(__dirname, './linux'),
+    resolve(__dirname, './windows')
+]
 
-    equal(a.length, b.length)
+if (dirsArg.startsWith('-')) {
+    if(dirsArg.startsWith('--dir')) {
+        if(dirsArg.startsWith('--dir=')) {
+            dirs = dirsArg.substring(6).split(',').map(path => resolve(path))
+        }
+        else {
+            process.exitCode = 1
+            console.error(`at least one directory must be specified`)
+            return
+        }
+    }
+    else {
+        process.exitCode = 1
+        console.error(`unknown flag: ${dirsArg}`)
+        return
+    }
+}
 
-    for (let i = 0; i < a.length; i++) {
-        const [ errnoA, [ codeA, descA ] ] = a[i]
-        const [ errnoB, [ codeB, descB ] ] = b[i]
+dirs
+    .map(d => readdirSync(d).map(f => resolve(d, f)))
+    .map(files => files.map(require))
+    .forEach(maps => 
+        maps.forEach((map, i) => {
+            if (i === 0) return
 
-        equal(errnoA, errnoB)
-        equal(codeA, codeB)
-        equal(descA, descB)
+            compare(map, maps[i - 1])
+        })
+    )
 
-        console.log(`${osName}: ${errnoA} [${codeA}] ${descA} ✅`)
+function compare(a, b) {
+    console.log(`\nCompare\nA: "${a.os}"\nB: "${b.os}"\n`)
+
+    notEqual(a, b, 'maps should not be reference-equal')
+    equal(a.length, b.length, 'maps should have the same number of items')
+
+    a = new Map(a)
+    b = new Map(b)
+
+    for (const errno of a.keys()) {
+        const [ codeA, descA ] = a.get(errno)
+        const [ codeB, descB ] = b.get(errno)
+
+        equal(
+            codeA, codeB, 
+            `errno ${errno} should represent the same code on both systems, got "${codeA}" !== "${codeB}"`
+        )
+        equal(
+            descA, descB, 
+            `errno ${errno} should represent the same description on both systems, got "${descA}" !== "${descB}"`
+        )
+
+        console.log(`${errno} => [${codeA}]: ${descA} ✅`)
     }
 }
 
